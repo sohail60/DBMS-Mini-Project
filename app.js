31a52445307a2c8ddb7be74c547d2252a5936dcf
sohail60 <sohailatwork10@gmail.com>
2023-04-17 21:27:34 +0530

    Functionality Added

--- app.js
+++ app.js
@@ -1,63 +1,171 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 // import con from './database'
-
-const app=express();
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
-
-app.set('view engine','ejs');
-
-const mysql=require("mysql2");
-const dotenv=require("dotenv");
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 dotenv.config();
 
-app.get("/",function(req,res){
-    console.log("working");
-    res.render("index.ejs");
-})
+const app = express();
 
-app.post('/insert',function(req,res){
-    var con =mysql.createConnection({
-  host:'127.0.0.1',
-  user:'root',
-  password:'',
-  database:'student_registration'
-  // host:process.env.MYSQL_HOST,
-  // user:process.env.MYSQL_USER,
-  // password:process.env.MYSQL_PASSWORD,
-  // database:process.env.MYSQL_DATABASE
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-    console.log('hi')
-    const name=req.body.name;
-    const roll=req.body.roll;
-    const branch=req.body.branch;
-    const year=req.body.year;
+app.set("view engine", "ejs");
 
-    con.connect(function(err) {
-        if (err){
+app.get("/", function (req, res) {
+  var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "student_registration",
+    // host:process.env.MYSQL_HOST,
+    // user:process.env.MYSQL_USER,
+    // password:process.env.MYSQL_PASSWORD,
+    // database:process.env.MYSQL_DATABASE
+  });
+
+  con.connect(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Inside /");
+      const sql = "SELECT * FROM student";
+      con.query(sql, function (err, result) {
+        if (err) {
           console.log(err);
           return;
         } else {
-            console.log("Connected!");
-            const sql = "INSERT INTO student (name, roll, branch, year) VALUES (?, ?, ?, ?)";
-            const values = [name, roll, branch, year];
-            con.query(sql, values, function (err, result) {
-              if (err){
-                console.log(err);
-                return;
-              } else {
-              console.log("Result: " + result);
-              res.redirect('/');
-              }
-            });
-          }
+          res.render("index.ejs", { list: result });
+        }
       });
-})
+    }
+  });
+});
 
+// INSERT
 
-app.listen(process.env.PORT || 3000,function(){
-    console.log("Server is running on port 3000");
+app.post("/submit", function (req, res) {
+  var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "student_registration",
+    // host:process.env.MYSQL_HOST,
+    // user:process.env.MYSQL_USER,
+    // password:process.env.MYSQL_PASSWORD,
+    // database:process.env.MYSQL_DATABASE
+  });
+
+  const name = req.body.name;
+  const roll = req.body.roll;
+  const branch = req.body.branch;
+  const year = req.body.year;
+
+  con.connect(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Inside insert");
+      const sql =
+        "INSERT INTO student (name, roll, branch, year) VALUES (?, ?, ?, ?)";
+      const values = [name, roll, branch, year];
+      con.query(sql, values, function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          res.redirect("/");
+        }
+      });
+    }
+  });
+});
+
+// DELETE
+
+app.post("/delete", function (req, res) {
+  var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "student_registration",
+    // host:process.env.MYSQL_HOST,
+    // user:process.env.MYSQL_USER,
+    // password:process.env.MYSQL_PASSWORD,
+    // database:process.env.MYSQL_DATABASE
+  });
+
+  const id = req.body.delete;
+  console.log("printing");
+  console.log(id);
+  console.log(req.body);
+  console.log("printing");
+
+  con.connect(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Inside Delete");
+      const sql = "DELETE from student WHERE Roll=?";
+      const values = [id];
+      con.query(sql, values, function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          res.redirect("/");
+        }
+      });
+    }
+  });
+});
+
+// UPDATE
+
+app.post("/update", function (req, res) {
+  var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "student_registration",
+    // host:process.env.MYSQL_HOST,
+    // user:process.env.MYSQL_USER,
+    // password:process.env.MYSQL_PASSWORD,
+    // database:process.env.MYSQL_DATABASE
+  });
+
+  const name = req.body.name;
+  const roll = req.body.roll;
+  const branch = req.body.branch;
+  const year = req.body.year;
+  console.log(req.body);
+
+  con.connect(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log("Inside Update");
+
+      const sql =
+        "REPLACE INTO student (Name,Roll,Branch,Year) VALUES (?,?,?,?)";
+      const values = [name, roll, branch, year];
+      con.query(sql, values, function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          res.redirect("/");
+        }
+      });
+    }
+  });
+});
+
+// LISTENING
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000");
 });
---
+++ public/server.js
@@ -0,0 +1,9 @@
+console.log('cnct')
+
+const deleteBtn=document.getElementById('delete');
+const updateBtn=document.getElementById('update');
+const insertBtn=document.getElementById('insert');
+
+deleteBtn.addEventListener('click',function(){
+    console.log('clicked');
+})
